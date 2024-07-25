b4d1e4fe509adb71ff5a05b10bad46918d0f7c5f
Aaron Kirkbride <aaron@aaronkirkbride.com>
2024-07-25 23:52:52 +0100

    Fix eslint

--- app/src/utils/string.ts
+++ app/src/utils/string.ts
@@ -2,7 +2,7 @@ export function slugify(text: string): string {
   return text
     .toLowerCase()
     .replaceAll(/\s+/g, '-') // Replace spaces with -
-    .replaceAll(/[^\w\-]+/g, '') // Remove all non-word chars
+    .replaceAll(/[^\w-]+/g, '') // Remove all non-word chars
     .replaceAll(/--+/g, '-') // Replace multiple - with single -
     .replace(/-+$/, ''); // Remove trailing - at the end
 }
